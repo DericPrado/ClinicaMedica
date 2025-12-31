aa22e919eefc8fb40be75d483a1009de52b1c0dc
= <=>
2025-12-31 11:12:29 -0300

    Adicionando injecao de dependencia

--- ClinicaMedicaAPI/ClinicaMedicaAPI/Program.cs
+++ ClinicaMedicaAPI/ClinicaMedicaAPI/Program.cs
@@ -1,3 +1,10 @@
+using ClinicaMedicaAPI.Infra;
+using ClinicaMedicaAPI.Modelos.Interfaces.Infra;
+using ClinicaMedicaAPI.Modelos.Interfaces.Services;
+using ClinicaMedicaAPI.Modelos.Interfaces.UseCases.Medico;
+using ClinicaMedicaAPI.Services;
+using ClinicaMedicaAPI.UseCases;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IRepositorioMedico, RepositorioMedico>();
+builder.Services.AddScoped<IMedicoService, MedicoService>();
+builder.Services.AddScoped<IAtualizaMedicoUseCase, AtualizaMedicoUseCase>();
+builder.Services.AddScoped<IDeletaMedicoUseCase, DeletaMedicoUseCase>();
+builder.Services.AddScoped<IRegistraMedicoUseCase, RegistraMedicoUseCase>();
+builder.Services.AddScoped<IRecuperaMedicoUseCase, RecuperaMedicoUseCase>();
 
 var app = builder.Build();
 
