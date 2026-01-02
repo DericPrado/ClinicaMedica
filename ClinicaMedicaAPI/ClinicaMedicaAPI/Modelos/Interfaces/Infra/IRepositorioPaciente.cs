cbf15ce834dd05b6c94b2eb0b4f4a1feabcd9e3a
= <=>
2026-01-02 16:57:53 -0300

    Criando repositorio dos pacientes

---
+++ ClinicaMedicaAPI/ClinicaMedicaAPI/Modelos/Interfaces/Infra/IRepositorioPaciente.cs
@@ -0,0 +1,14 @@
+﻿using ClinicaMedicaAPI.Modelos.Entidades;
+
+namespace ClinicaMedicaAPI.Modelos.Interfaces.Infra
+{
+    public interface IRepositorioPaciente
+    {
+        public Task<bool> Registrar(Paciente medico);
+        public Task<bool> Atualizar(Paciente request);
+        public Task<List<Paciente>> ListarPacientesAtivos();
+        public Task<Paciente> ListaPacientePorId(Guid id);
+        public Task<bool> DeletarPacientePorId(Guid id);
+        public Task<Guid> GerarId();
+    }
+}
