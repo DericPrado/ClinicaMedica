36f7d27b9e9b07f8c722b938f0c288f2647f243e
Deric <dericprado@gmail.com>
2026-01-03 08:34:51 -0300

    Realizando alguns ajustes

--- ClinicaMedicaAPI/ClinicaMedicaAPI/Modelos/Interfaces/Infra/IRepositorioPaciente.cs
+++ ClinicaMedicaAPI/ClinicaMedicaAPI/Modelos/Interfaces/Infra/IRepositorioPaciente.cs
@@ -4,8 +4,8 @@ namespace ClinicaMedicaAPI.Modelos.Interfaces.Infra
 {
     public interface IRepositorioPaciente
     {
-        public Task<bool> Registrar(Paciente medico);
-        public Task<bool> Atualizar(Paciente request);
+        public Task<bool> Registrar(Paciente paciente);
+        public Task<bool> Atualizar(Paciente paciente);
         public Task<List<Paciente>> ListarPacientesAtivos();
         public Task<Paciente> ListaPacientePorId(Guid id);
         public Task<bool> DeletarPacientePorId(Guid id);
---
+++ ClinicaMedicaAPI/ClinicaMedicaAPI/UseCases/Medico/AtualizaMedicoUseCase.cs
@@ -0,0 +1,21 @@
+﻿using ClinicaMedicaAPI.Modelos.DTOs.Medico;
+using ClinicaMedicaAPI.Modelos.Interfaces.Services;
+using ClinicaMedicaAPI.Modelos.Interfaces.UseCases.Medico;
+
+namespace ClinicaMedicaAPI.UseCases.Medico
+{
+    public class AtualizaMedicoUseCase : IAtualizaMedicoUseCase
+    {
+        private IMedicoService _medicoService;
+
+        public AtualizaMedicoUseCase (IMedicoService medicoService)
+        {
+            _medicoService = medicoService;
+        }
+
+        public async Task<bool> Executar(RequestAtualizaMedico request)
+        {
+            return await _medicoService.Atualizar(request);
+        }
+    }
+}
